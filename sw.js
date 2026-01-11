cffc30eb100b87107835386408699b7daed869cd
ramkseenivasan-lawfrnd <ramkseenivasan@gmail.com>
2026-01-11 06:35:43 +0530

    icons update

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-
 const CACHE_NAME = 'tnpsc-frnd-v1';
 const ASSETS = [
   '/',
   '/index.html',
   '/index.tsx',
-  '/manifest.json'
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
@@ -15,10 +16,39 @@ self.addEventListener('install', (event) => {
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Check if the request is for an icon or the manifest
+  if (event.request.url.includes('/icons/') || event.request.url.includes('manifest.json')) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const clonedResponse = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, clonedResponse);
+          });
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
